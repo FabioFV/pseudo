132f9dc61ac741644abb98fad0bf0576bb5d1448
loashirley <loa_shirley@hotmail.com>
2016-04-04 11:30:22 -0500

    Open option

--- src/ActionPerformer.java
+++ src/ActionPerformer.java
@@ -6,17 +6,60 @@ import java.util.regex.Pattern;
  * Created by Loammi on 4/4/2016.
  */
 public class ActionPerformer {
-    private final IDEGUI ide;    //instancia de TPEditor (la clase principal)
+    private final IDEGUI ide;
 
     public ActionPerformer(IDEGUI ide) {
-        this.ide = ide;    //guarda la instancia de la clase TPEditor
+        this.ide = ide;
     }
 
+    public void actionOpen() {
+        if (ide.documentHasChanged() == true) {    //si el documento esta marcado como modificado
 
+            int option = JOptionPane.showConfirmDialog(ide.getJFrame(), "¿Desea guardar los cambios?");
+            switch (option) {
+                case JOptionPane.YES_OPTION:     //si elige que si
+                    actionSave();               //guarda el archivo
+                    break;
+                case JOptionPane.CANCEL_OPTION:  //si elige cancelar
+                    return;                      //cancela esta operación
+                //en otro caso se continúa con la operación y no se guarda el documento actual
+            }
+        }
+        JFileChooser fc = getJFileChooser();
+        int state = fc.showOpenDialog(ide.getJFrame());
+
+        if (state == JFileChooser.APPROVE_OPTION) {    //si elige abrir el archivo
+            File f = fc.getSelectedFile();    //obtiene el archivo seleccionado
+
+            try {
+
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                ide.getJTextArea().read(br, null);
+                br.close();
+
+                ide.getJTextArea().getDocument().addUndoableEditListener(ide.getEventHandler());
+
+               ide.getUndoManager().die();    //se limpia el buffer del administrador de edición
+             //   ide.updateControls();          //se actualiza el estado de las opciones "Deshacer" y "Rehacer"
+
+                ide.getJFrame().setTitle("PSEUDO - " + f.getName());
+                ide.getJLabelFilePath().setText(shortPathName(f.getAbsolutePath()));
+                ide.getJLabelFileSize().setText(roundFileSize(f.length()));
+                ide.setCurrentFile(f);
+                ide.setDocumentChanged(false);
+            } catch (IOException ex) {    //en caso de que ocurra una excepción
+                //presenta un dialogo modal con alguna información de la excepción
+                JOptionPane.showMessageDialog(ide.getJFrame(),
+                        ex.getMessage(),
+                        ex.toString(),
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
     public void actionSave() {
         if (ide.getCurrentFile() == null) {    //si no hay un archivo asociado al documento actual
             actionSaveAs();    //invoca el método actionSaveAs()
-        } else if (ide.documentHasChanged() == true) {    //si el documento esta marcado como modificado
+        } else if (ide.documentHasChanged()) {    //si el documento esta marcado como modificado
             try {
                 BufferedWriter bw = new BufferedWriter(new FileWriter(ide.getCurrentFile()));
                 ide.getJTextArea().write(bw);
@@ -34,14 +77,12 @@ public class ActionPerformer {
     public void actionSaveAs() {
         JFileChooser fc = getJFileChooser();    //obtiene un JFileChooser
 
-
         int state = fc.showSaveDialog(ide.getJFrame());
         if (state == JFileChooser.APPROVE_OPTION) {    //si elige guardar en el archivo
             File f = fc.getSelectedFile();    //obtiene el archivo seleccionado
 
             try {
                 BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-
                 ide.getJTextArea().write(bw);
                 bw.close();
 
@@ -52,10 +93,7 @@ public class ActionPerformer {
                 ide.getJLabelFilePath().setText(shortPathName(f.getAbsolutePath()));
                 //muestra el tamaño del archivo guardado
                 ide.getJLabelFileSize().setText(roundFileSize(f.length()));
-
-                //establece el archivo guardado como el archivo actual
                 ide.setCurrentFile(f);
-                //marca el estado del documento como no modificado
                 ide.setDocumentChanged(false);
             } catch (IOException ex) {    //en caso de que ocurra una excepción
                 //presenta un dialogo modal con alguna información de la excepción
@@ -75,7 +113,7 @@ public class ActionPerformer {
         JFileChooser fc = new JFileChooser();                     //construye un JFileChooser
         fc.setDialogTitle("PSEUDO - Elige un archivo:");    //se le establece un título
         fc.setMultiSelectionEnabled(false);                       //desactiva la multi-selección
-        //  fc.setFileFilter(textFileFilter);                         //aplica un filtro de extensiones
+       // fc.setFileFilter(textFileFilter);                         //aplica un filtro de extensiones
         return fc;    //retorna el JFileChooser
     }
 
@@ -87,6 +125,7 @@ public class ActionPerformer {
 
         @Override
         public boolean accept(File f) {
+
             return f.isDirectory() || f.getName().toLowerCase().endsWith("txt");
         }
 
@@ -119,6 +158,5 @@ public class ActionPerformer {
         //retorna el tamaño del archivo redondeado
         return (length < 1024) ? length + " bytes" : (length / 1024) + " Kbytes";
     }
-
 
 }
